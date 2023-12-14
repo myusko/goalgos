c96c5a6f8395ed21630b81240801f826351bdfe2
Mykhailo Yusko <myusko@protonmail.com>
2023-12-14 17:56:56 +0200

    add url

--- arithmetic.go
+++ arithmetic.go
@@ -1,5 +1,7 @@
 package goalgos
 
+// Interested gif https://en.wikipedia.org/wiki/Arithmetic_progression#/media/File:Animated_proof_for_the_formula_giving_the_sum_of_the_first_integers_1+2+...+n.gif
+
 func IsArithmeticSequence(numbers []int) bool {
 	diff := numbers[0] - numbers[1]
 
